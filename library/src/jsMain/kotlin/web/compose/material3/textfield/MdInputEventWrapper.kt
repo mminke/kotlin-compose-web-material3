ec9a75b1f4ea123a564dcfbc0a810bbc5305293d
mminke <m.minke@kataru.nl>
2023-04-27 18:36:25 +0200

    feat(input): remove data property

--- library/src/jsMain/kotlin/web/compose/material3/textfield/MdInputEventWrapper.kt
+++ library/src/jsMain/kotlin/web/compose/material3/textfield/MdInputEventWrapper.kt
@@ -4,8 +4,6 @@ import androidx.compose.web.events.SyntheticEvent
 import org.w3c.dom.events.EventTarget
 
 class MdInputEventWrapper internal constructor(private val syntheticEvent: SyntheticEvent<EventTarget>) {
-    val data: String?
-        get() = syntheticEvent.nativeEvent.target.asDynamic().value?.unsafeCast<String>()
     val value: String?
         get() = syntheticEvent.nativeEvent.target.asDynamic().value?.unsafeCast<String>()
 }
