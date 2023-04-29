17477f9553467a1602ca88fc5d68ab31244ec939
mminke <m.minke@kataru.nl>
2023-04-29 15:38:41 +0200

    feat(filedragdrop): Use style variables from MD3

--- library/src/jsMain/kotlin/web/compose/extras/fileupload/OutlinedFileDragDropArea.kt
+++ library/src/jsMain/kotlin/web/compose/extras/fileupload/OutlinedFileDragDropArea.kt
@@ -1,17 +1,23 @@
 package web.compose.extras.fileupload
 
 import androidx.compose.runtime.Composable
+import org.jetbrains.compose.web.css.CSSColorValue
+import org.jetbrains.compose.web.css.CSSNumeric
+import org.jetbrains.compose.web.css.CSSStyleVariable
 import org.jetbrains.compose.web.css.Color
 import org.jetbrains.compose.web.css.LineStyle
 import org.jetbrains.compose.web.css.Style
 import org.jetbrains.compose.web.css.StyleSheet
 import org.jetbrains.compose.web.css.border
+import org.jetbrains.compose.web.css.borderRadius
 import org.jetbrains.compose.web.css.color
 import org.jetbrains.compose.web.css.maxHeight
 import org.jetbrains.compose.web.css.minHeight
 import org.jetbrains.compose.web.css.overflow
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.css.style
+import org.jetbrains.compose.web.css.value
+import org.jetbrains.compose.web.css.width
 import org.jetbrains.compose.web.dom.Div
 import org.w3c.dom.asList
 import org.w3c.files.File
@@ -22,20 +28,24 @@ object DragDropAreaStyleSheet : StyleSheet() {
             overflow("hidden")
             border {
                 style(LineStyle.Solid)
-                color(Color.lightgray)
+                color(CSSStyleVariable<CSSColorValue>("md-sys-color-outline").value(Color.red))
+                width(1.px)
             }
+            borderRadius(CSSStyleVariable<CSSNumeric>("md-outlined-button-container-shape").value(4.px))
         }
         className("dragdroparea") + hover style {
             border {
                 style(LineStyle.Dashed)
-                color(Color.darkgray)
+                color(CSSStyleVariable<CSSColorValue>("md-sys-color-outline").value(Color.red))
+                width(1.px)
             }
+            borderRadius(CSSStyleVariable<CSSNumeric>("md-outlined-button-container-shape").value(4.px))
         }
     }
 }
 
 @Composable
-fun FileDragDropArea(
+fun OutlinedFileDragDropArea(
     onDrop: ((List<File>) -> Unit)? = null,
     content: @Composable() (() -> Unit)? = null
 ) {
@@ -46,7 +56,6 @@ fun FileDragDropArea(
             minHeight(100.px)
             maxHeight(100.px)
         }
-        //TODO: Change style when draging over the box
         onDragEnter { it.stopPropagation(); it.preventDefault() }
         onDragOver { it.stopPropagation(); it.preventDefault() }
         onDrop {
