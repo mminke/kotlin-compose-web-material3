d4738e660d7a78e6b5a602f7e6b4f34a31c8823b
mminke <mminke@bol.com>
2023-05-04 12:35:00 +0200

    feat(theming): Add ColorScheme functionality

--- example/src/jsMain/kotlin/web/compose/material3/example/main.kt
+++ example/src/jsMain/kotlin/web/compose/material3/example/main.kt
@@ -1,42 +1,16 @@
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import org.jetbrains.compose.web.css.CSSColorValue
-import org.jetbrains.compose.web.css.CSSStyleVariable
+import androidx.compose.runtime.*
+import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.css.Color.lightgray
-import org.jetbrains.compose.web.css.LineStyle
-import org.jetbrains.compose.web.css.Style
-import org.jetbrains.compose.web.css.StyleSheet
-import org.jetbrains.compose.web.css.border
-import org.jetbrains.compose.web.css.color
-import org.jetbrains.compose.web.css.fontSize
-import org.jetbrains.compose.web.css.padding
-import org.jetbrains.compose.web.css.px
-import org.jetbrains.compose.web.css.style
-import org.jetbrains.compose.web.css.width
-import org.jetbrains.compose.web.dom.A
-import org.jetbrains.compose.web.dom.Div
-import org.jetbrains.compose.web.dom.H1
-import org.jetbrains.compose.web.dom.H2
-import org.jetbrains.compose.web.dom.H3
-import org.jetbrains.compose.web.dom.Label
-import org.jetbrains.compose.web.dom.P
-import org.jetbrains.compose.web.dom.Span
-import org.jetbrains.compose.web.dom.Text
+import org.jetbrains.compose.web.dom.*
 import org.jetbrains.compose.web.renderComposable
 import org.w3c.files.File
 import web.compose.extras.Column
 import web.compose.extras.Row
 import web.compose.extras.fileupload.FilledFileInput
 import web.compose.extras.fileupload.OutlinedFileDragDropArea
+import web.compose.material3.ColorScheme
 import web.compose.material3.MaterialTheme
-import web.compose.material3.buttons.ElevatedButton
-import web.compose.material3.buttons.FilledButton
-import web.compose.material3.buttons.OutlinedButton
-import web.compose.material3.buttons.TextButton
-import web.compose.material3.buttons.TonalButton
+import web.compose.material3.buttons.*
 import web.compose.material3.dialog.Dialog
 import web.compose.material3.dialog.onCancel
 import web.compose.material3.dialog.onClose
@@ -56,30 +30,10 @@ import web.compose.material3.list.headline
 import web.compose.material3.list.supportingText
 import web.compose.material3.navigationbar.NavigationBar
 import web.compose.material3.navigationdrawer.NavigationDrawer
-import web.compose.material3.select.FilledSelect
-import web.compose.material3.select.OutlinedSelect
-import web.compose.material3.select.SelectOption
-import web.compose.material3.select.headline
-import web.compose.material3.select.label
-import web.compose.material3.select.onChange
-import web.compose.material3.select.supportingText
-import web.compose.material3.select.value
+import web.compose.material3.select.*
 import web.compose.material3.slot
-import web.compose.material3.textfield.FilledTextField
-import web.compose.material3.textfield.OutlinedTextField
-import web.compose.material3.textfield.TextFieldType.EMAIL
-import web.compose.material3.textfield.TextFieldType.NUMBER
-import web.compose.material3.textfield.TextFieldType.PASSWORD
-import web.compose.material3.textfield.TextFieldType.SEARCH
-import web.compose.material3.textfield.TextFieldType.URL
-import web.compose.material3.textfield.disabled
-import web.compose.material3.textfield.error
-import web.compose.material3.textfield.errorText
-import web.compose.material3.textfield.label
-import web.compose.material3.textfield.onInput
-import web.compose.material3.textfield.required
-import web.compose.material3.textfield.type
-import web.compose.material3.textfield.value
+import web.compose.material3.textfield.*
+import web.compose.material3.textfield.TextFieldType.*
 import kotlin.math.roundToInt
 
 data class ListDataItem(
@@ -92,86 +46,27 @@ val listData = listOf(
 )
 
 
-object MdSysColorVariables {
-    val mdSysColorPrimary = CSSStyleVariable<CSSColorValue>("md-sys-color-primary")
-    val mdSysColorPrimaryContainer = CSSStyleVariable<CSSColorValue>("md-sys-color-primary-container")
-    val mdSysColorOnPrimary = CSSStyleVariable<CSSColorValue>("md-sys-color-on-primary")
-    val mdSysColorOnPrimaryContainer = CSSStyleVariable<CSSColorValue>("md-sys-color-on-primary-container")
-    val mdSysColorSecondary = CSSStyleVariable<CSSColorValue>("md-sys-color-secondary")
-    val mdSysColorSecondaryContainer = CSSStyleVariable<CSSColorValue>("md-sys-color-secondary-container")
-    val mdSysColorOnSecondary = CSSStyleVariable<CSSColorValue>("md-sys-color-on-secondary")
-    val mdSysColorOnSecondaryContainer = CSSStyleVariable<CSSColorValue>("md-sys-color-on-secondary-container")
-    val mdSysColorTertiary = CSSStyleVariable<CSSColorValue>("md-sys-color-tertiary")
-    val mdSysColorTertiaryContainer = CSSStyleVariable<CSSColorValue>("md-sys-color-tertiary-container")
-    val mdSysColorOnTertiary = CSSStyleVariable<CSSColorValue>("md-sys-color-on-tertiary")
-    val mdSysColorOnTertiaryContainer = CSSStyleVariable<CSSColorValue>("md-sys-color-on-tertiary-container")
-    val mdSysColorError = CSSStyleVariable<CSSColorValue>("md-sys-color-error")
-    val mdSysColorErrorContainer = CSSStyleVariable<CSSColorValue>("md-sys-color-error-container")
-    val mdSysColorOnError = CSSStyleVariable<CSSColorValue>("md-sys-color-on-error")
-    val mdSysColorOnErrorContainer = CSSStyleVariable<CSSColorValue>("md-sys-color-on-error-container")
-    val mdSysColorOutline = CSSStyleVariable<CSSColorValue>("md-sys-color-outline")
-    val mdSysColorBackground = CSSStyleVariable<CSSColorValue>("md-sys-color-background")
-    val mdSysColorOnBackground = CSSStyleVariable<CSSColorValue>("md-sys-color-on-background")
-    val mdSysColorSurface = CSSStyleVariable<CSSColorValue>("md-sys-color-surface")
-    val mdSysColorOnSurface = CSSStyleVariable<CSSColorValue>("md-sys-color-on-surface")
-    val mdSysColorSurfaceVariant = CSSStyleVariable<CSSColorValue>("md-sys-color-surface-variant")
-    val mdSysColorOnSurfaceVariant = CSSStyleVariable<CSSColorValue>("md-sys-color-on-surface-variant")
-    val mdSysColorInverseSurface = CSSStyleVariable<CSSColorValue>("md-sys-color-inverse-surface")
-    val mdSysColorInverseOnSurface = CSSStyleVariable<CSSColorValue>("md-sys-color-inverse-on-surface")
-    val mdSysColorInversePrimary = CSSStyleVariable<CSSColorValue>("md-sys-color-inverse-primary")
-    val mdSysColorShadow = CSSStyleVariable<CSSColorValue>("md-sys-color-shadow")
-    val mdSysColorSurfaceTint = CSSStyleVariable<CSSColorValue>("md-sys-color-surface-tint")
-    val mdSysColorOutlineVariant = CSSStyleVariable<CSSColorValue>("md-sys-color-outline-variant")
-    val mdSysColorScrim = CSSStyleVariable<CSSColorValue>("md-sys-color-scrim")
-}
-
-object GreenishColorScheme : StyleSheet() {
-    init {
-        universal style {
-//            MyVariables.mdSysColorPrimary(Color("#006A6A"))
-//            MyVariables.mdSysColorPrimaryContainer(Color("#6FF7F6"))
-//            MyVariables.mdSysColorOnPrimary(Color("#FFFFFF"))
-//            MyVariables.mdSysColorOnPrimaryContainer(Color("#002020"))
-//            MyVariables.mdSysColorSecondary(Color("#4A6363"))
-//            MyVariables.mdSysColorSecondaryContainer(Color("#CCE8E7"))
-//            MyVariables.mdSysColorOnSecondary(Color("#FFFFFF"))
-//            MyVariables.mdSysColorOnSecondaryContainer(Color("#051F1F"))
-//            MyVariables.mdSysColorTertiary(Color("#4B607C"))
-//            MyVariables.mdSysColorTertiaryContainer(Color("#D3E4FF"))
-//            MyVariables.mdSysColorOnTertiary(Color("#FFFFFF"))
-//            MyVariables.mdSysColorOnTertiaryContainer(Color("#041C35"))
-//            MyVariables.mdSysColorError(Color("#BA1A1A"))
-//            MyVariables.mdSysColorErrorContainer(Color("#FFDAD6"))
-//            MyVariables.mdSysColorOnError(Color("#FFFFFF"))
-//            MyVariables.mdSysColorOnErrorContainer(Color("#410002"))
-//            MyVariables.mdSysColorOutline(Color("#6F7979"))
-//            MyVariables.mdSysColorBackground(Color("#FAFDFC"))
-//            MyVariables.mdSysColorOnBackground(Color("#191C1C"))
-//            MyVariables.mdSysColorSurface(Color("#FAFDFC"))
-//            MyVariables.mdSysColorOnSurface(Color("#191C1C"))
-//            MyVariables.mdSysColorSurfaceVariant(Color("#DAE5E4"))
-//            MyVariables.mdSysColorOnSurfaceVariant(Color("#3F4948"))
-//            MyVariables.mdSysColorInverseSurface(Color("#2D3131"))
-//            MyVariables.mdSysColorInverseOnSurface(Color("#EFF1F0"))
-//            MyVariables.mdSysColorInversePrimary(Color("#4CDADA"))
-//            MyVariables.mdSysColorShadow(Color("#000000"))
-//            MyVariables.mdSysColorSurfaceTint(Color("#006A6A"))
-//            MyVariables.mdSysColorOutlineVariant(Color("#BEC9C8"))
-//            MyVariables.mdSysColorScrim(Color("#000000"))
-        }
-    }
-}
-
-
 fun main() {
 
     renderComposable(rootElementId = "root") {
-        Style(GreenishColorScheme)
-
         var textFieldValue by remember { mutableStateOf("Initial text field value") }
         var checkboxStatus by remember { mutableStateOf(false) }
+        var colorScheme by remember { mutableStateOf<ColorScheme?>(null) }
 
-        MaterialTheme {
+        MaterialTheme(colorScheme) {
+            NavigationBar {
+                Text("Switch theme")
+                Switch({
+                    selected(colorScheme != null)
+                    onClick {
+                        if (colorScheme == null)
+                            colorScheme = greenishColorScheme
+                        else
+                            colorScheme = null
+                    }
+                })
+            }
+
             H1 { Text("Kotlin HTML - Material Design theme 3 Web Components Examples") }
 
             P {
@@ -210,7 +105,7 @@ fun main() {
                         OutlinedButton({
                             onClick { textFieldValue = "outlined button clicked" }
                             style { padding(5.px) }
-                        }) { Text("Outlined Button")}
+                        }) { Text("Outlined Button") }
                         ElevatedButton({
                             onClick { textFieldValue = "elevated button clicked" }
                             style { padding(5.px) }
@@ -367,30 +262,30 @@ fun main() {
                             onClick { checkboxStatus = !checkboxStatus }
                         })
 
-                        H3 { Text("Slider")}
+                        H3 { Text("Slider") }
                         Slider({
                             min = 100
                             max = 200
                             step = 2
                             withLabel()
-                        }) {  }
+                        }) { }
 
-                        H3 { Text("Radio")}
-                        Row { Radio({ id("r1"); name = "radio1"; value = "o1"}); Label("r1") { Text("Option 1") } }
-                        Row { Radio({ id("r2"); name = "radio1"; value = "o2"}); Label("r2") { Text("Option 2") }}
-                        Row { Radio({ id("r3"); name = "radio1"; value = "o3"}); Label("r3") { Text("Option 3") }}
+                        H3 { Text("Radio") }
+                        Row { Radio({ id("r1"); name = "radio1"; value = "o1" }); Label("r1") { Text("Option 1") } }
+                        Row { Radio({ id("r2"); name = "radio1"; value = "o2" }); Label("r2") { Text("Option 2") } }
+                        Row { Radio({ id("r3"); name = "radio1"; value = "o3" }); Label("r3") { Text("Option 3") } }
 
-                        H3 { Text("Select")}
+                        H3 { Text("Select") }
                         var selectedValue by remember { mutableStateOf("") }
 
                         FilledSelect({
                             label = "Select label"
                             supportingText = "Selected value $selectedValue"
                             onChange {
-                                selectedValue = it.currentTarget?.asDynamic()?.value?: ""
+                                selectedValue = it.currentTarget?.asDynamic()?.value ?: ""
                             }
                         }) {
-                            SelectOption {  }
+                            SelectOption { }
                             SelectOption({ value = "MD1"; headline = "Material Design 1" })
                             SelectOption({ value = "MD2"; headline = "Material Design 2" })
                             SelectOption({ value = "MD3"; headline = "Material Design 3" })
@@ -400,10 +295,10 @@ fun main() {
                             label = "Select label"
                             supportingText = "Selected value $selectedValue"
                             onChange {
-                                selectedValue = it.currentTarget?.asDynamic()?.value?: ""
+                                selectedValue = it.currentTarget?.asDynamic()?.value ?: ""
                             }
                         }) {
-                            SelectOption {  }
+                            SelectOption { }
                             SelectOption({ value = "MD1"; headline = "Material Design 1" })
                             SelectOption({ value = "MD2"; headline = "Material Design 2" })
                             SelectOption({ value = "MD3"; headline = "Material Design 3" })
@@ -456,10 +351,10 @@ fun main() {
                     var selectedFiles by remember { mutableStateOf<List<File>>(emptyList()) }
                     Column {
                         H2 { Text("File input button") }
-                        FilledFileInput( {
+                        FilledFileInput({
                             if (it == null) return@FilledFileInput
                             selectedFiles = it
-                        }) {Text("Upload files...")}
+                        }) { Text("Upload files...") }
 
                         FileList(selectedFiles)
                     }
@@ -551,3 +446,37 @@ fun formatUnits(base: Int, units: List<String>): (Double, Int) -> String {
 
 private val formatBytesImpl = formatUnits(1024, listOf("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"))
 fun formatBytes(bytes: Int): String = formatBytesImpl(bytes.toDouble(), 2)
+
+
+val greenishColorScheme = ColorScheme(
+        sysColorPrimary = Color("#006A6A"),
+        sysColorPrimaryContainer = Color("#6FF7F6"),
+        sysColorOnPrimary = Color("#FFFFFF"),
+        sysColorOnPrimaryContainer = Color("#002020"),
+        sysColorSecondary = Color("#4A6363"),
+        sysColorSecondaryContainer = Color("#CCE8E7"),
+        sysColorOnSecondary = Color("#FFFFFF"),
+        sysColorOnSecondaryContainer = Color("#051F1F"),
+        sysColorTertiary = Color("#4B607C"),
+        sysColorTertiaryContainer = Color("#D3E4FF"),
+        sysColorOnTertiary = Color("#FFFFFF"),
+        sysColorOnTertiaryContainer = Color("#041C35"),
+        sysColorError = Color("#BA1A1A"),
+        sysColorErrorContainer = Color("#FFDAD6"),
+        sysColorOnError = Color("#FFFFFF"),
+        sysColorOnErrorContainer = Color("#410002"),
+        sysColorOutline = Color("#6F7979"),
+        sysColorBackground = Color("#FAFDFC"),
+        sysColorOnBackground = Color("#191C1C"),
+        sysColorSurface = Color("#FAFDFC"),
+        sysColorOnSurface = Color("#191C1C"),
+        sysColorSurfaceVariant = Color("#DAE5E4"),
+        sysColorOnSurfaceVariant = Color("#3F4948"),
+        sysColorInverseSurface = Color("#2D3131"),
+        sysColorInverseOnSurface = Color("#EFF1F0"),
+        sysColorInversePrimary = Color("#4CDADA"),
+        sysColorShadow = Color("#000000"),
+        sysColorSurfaceTint = Color("#006A6A"),
+        sysColorOutlineVariant = Color("#BEC9C8"),
+        sysColorScrim = Color("#000000")
+)
--- library/src/jsMain/kotlin/web/compose/material3/MaterialTheme.kt
+++ library/src/jsMain/kotlin/web/compose/material3/MaterialTheme.kt
@@ -1,16 +1,120 @@
 package web.compose.material3
 
 import androidx.compose.runtime.Composable
+import org.jetbrains.compose.web.css.CSSColorValue
+import org.jetbrains.compose.web.css.CSSStyleVariable
+import org.jetbrains.compose.web.css.Style
+import org.jetbrains.compose.web.css.StyleSheet
 
 @JsName("require")
 external fun jsRequire(module: String): dynamic
 
-typealias ThemeContent = @Composable MaterialTheme.() -> Unit
-
-class MaterialTheme
-
 @Composable
-fun MaterialTheme(themeContent: ThemeContent) {
-    val materialTheme = MaterialTheme()
-    materialTheme.themeContent()
+fun MaterialTheme(colorScheme: ColorScheme? = null, content: @Composable () -> Unit) {
+    if(colorScheme != null) {
+        Style(colorScheme.asStylesheet())
+    }
+
+    content()
+}
+
+data class ColorScheme(
+        val sysColorPrimary: CSSColorValue,
+        val sysColorPrimaryContainer: CSSColorValue,
+        val sysColorOnPrimary: CSSColorValue,
+        val sysColorOnPrimaryContainer: CSSColorValue,
+        val sysColorSecondary: CSSColorValue,
+        val sysColorSecondaryContainer: CSSColorValue,
+        val sysColorOnSecondary: CSSColorValue,
+        val sysColorOnSecondaryContainer: CSSColorValue,
+        val sysColorTertiary: CSSColorValue,
+        val sysColorTertiaryContainer: CSSColorValue,
+        val sysColorOnTertiary: CSSColorValue,
+        val sysColorOnTertiaryContainer: CSSColorValue,
+        val sysColorError: CSSColorValue,
+        val sysColorErrorContainer: CSSColorValue,
+        val sysColorOnError: CSSColorValue,
+        val sysColorOnErrorContainer: CSSColorValue,
+        val sysColorOutline: CSSColorValue,
+        val sysColorBackground: CSSColorValue,
+        val sysColorOnBackground: CSSColorValue,
+        val sysColorSurface: CSSColorValue,
+        val sysColorOnSurface: CSSColorValue,
+        val sysColorSurfaceVariant: CSSColorValue,
+        val sysColorOnSurfaceVariant: CSSColorValue,
+        val sysColorInverseSurface: CSSColorValue,
+        val sysColorInverseOnSurface: CSSColorValue,
+        val sysColorInversePrimary: CSSColorValue,
+        val sysColorShadow: CSSColorValue,
+        val sysColorSurfaceTint: CSSColorValue,
+        val sysColorOutlineVariant: CSSColorValue,
+        val sysColorScrim: CSSColorValue
+) {
+    fun asStylesheet(): StyleSheet = StyleSheet().apply {
+        universal style {
+            MdSysColorVariables.mdSysColorPrimary(sysColorPrimary)
+            MdSysColorVariables.mdSysColorPrimaryContainer(sysColorPrimaryContainer)
+            MdSysColorVariables.mdSysColorOnPrimary(sysColorOnPrimary)
+            MdSysColorVariables.mdSysColorOnPrimaryContainer(sysColorOnPrimaryContainer)
+            MdSysColorVariables.mdSysColorSecondary(sysColorSecondary)
+            MdSysColorVariables.mdSysColorSecondaryContainer(sysColorSecondaryContainer)
+            MdSysColorVariables.mdSysColorOnSecondary(sysColorOnSecondary)
+            MdSysColorVariables.mdSysColorOnSecondaryContainer(sysColorOnSecondaryContainer)
+            MdSysColorVariables.mdSysColorTertiary(sysColorTertiary)
+            MdSysColorVariables.mdSysColorTertiaryContainer(sysColorTertiaryContainer)
+            MdSysColorVariables.mdSysColorOnTertiary(sysColorOnTertiary)
+            MdSysColorVariables.mdSysColorOnTertiaryContainer(sysColorOnTertiaryContainer)
+            MdSysColorVariables.mdSysColorError(sysColorError)
+            MdSysColorVariables.mdSysColorErrorContainer(sysColorErrorContainer)
+            MdSysColorVariables.mdSysColorOnError(sysColorOnError)
+            MdSysColorVariables.mdSysColorOnErrorContainer(sysColorOnErrorContainer)
+            MdSysColorVariables.mdSysColorOutline(sysColorOutline)
+            MdSysColorVariables.mdSysColorBackground(sysColorBackground)
+            MdSysColorVariables.mdSysColorOnBackground(sysColorOnBackground)
+            MdSysColorVariables.mdSysColorSurface(sysColorSurface)
+            MdSysColorVariables.mdSysColorOnSurface(sysColorOnSurface)
+            MdSysColorVariables.mdSysColorSurfaceVariant(sysColorSurfaceVariant)
+            MdSysColorVariables.mdSysColorOnSurfaceVariant(sysColorOnSurfaceVariant)
+            MdSysColorVariables.mdSysColorInverseSurface(sysColorInverseSurface)
+            MdSysColorVariables.mdSysColorInverseOnSurface(sysColorInverseOnSurface)
+            MdSysColorVariables.mdSysColorInversePrimary(sysColorInversePrimary)
+            MdSysColorVariables.mdSysColorShadow(sysColorShadow)
+            MdSysColorVariables.mdSysColorSurfaceTint(sysColorSurfaceTint)
+            MdSysColorVariables.mdSysColorOutlineVariant(sysColorOutlineVariant)
+            MdSysColorVariables.mdSysColorScrim(sysColorScrim)
+        }
+    }
+}
+
+object MdSysColorVariables {
+    val mdSysColorPrimary = CSSStyleVariable<CSSColorValue>("md-sys-color-primary")
+    val mdSysColorPrimaryContainer = CSSStyleVariable<CSSColorValue>("md-sys-color-primary-container")
+    val mdSysColorOnPrimary = CSSStyleVariable<CSSColorValue>("md-sys-color-on-primary")
+    val mdSysColorOnPrimaryContainer = CSSStyleVariable<CSSColorValue>("md-sys-color-on-primary-container")
+    val mdSysColorSecondary = CSSStyleVariable<CSSColorValue>("md-sys-color-secondary")
+    val mdSysColorSecondaryContainer = CSSStyleVariable<CSSColorValue>("md-sys-color-secondary-container")
+    val mdSysColorOnSecondary = CSSStyleVariable<CSSColorValue>("md-sys-color-on-secondary")
+    val mdSysColorOnSecondaryContainer = CSSStyleVariable<CSSColorValue>("md-sys-color-on-secondary-container")
+    val mdSysColorTertiary = CSSStyleVariable<CSSColorValue>("md-sys-color-tertiary")
+    val mdSysColorTertiaryContainer = CSSStyleVariable<CSSColorValue>("md-sys-color-tertiary-container")
+    val mdSysColorOnTertiary = CSSStyleVariable<CSSColorValue>("md-sys-color-on-tertiary")
+    val mdSysColorOnTertiaryContainer = CSSStyleVariable<CSSColorValue>("md-sys-color-on-tertiary-container")
+    val mdSysColorError = CSSStyleVariable<CSSColorValue>("md-sys-color-error")
+    val mdSysColorErrorContainer = CSSStyleVariable<CSSColorValue>("md-sys-color-error-container")
+    val mdSysColorOnError = CSSStyleVariable<CSSColorValue>("md-sys-color-on-error")
+    val mdSysColorOnErrorContainer = CSSStyleVariable<CSSColorValue>("md-sys-color-on-error-container")
+    val mdSysColorOutline = CSSStyleVariable<CSSColorValue>("md-sys-color-outline")
+    val mdSysColorBackground = CSSStyleVariable<CSSColorValue>("md-sys-color-background")
+    val mdSysColorOnBackground = CSSStyleVariable<CSSColorValue>("md-sys-color-on-background")
+    val mdSysColorSurface = CSSStyleVariable<CSSColorValue>("md-sys-color-surface")
+    val mdSysColorOnSurface = CSSStyleVariable<CSSColorValue>("md-sys-color-on-surface")
+    val mdSysColorSurfaceVariant = CSSStyleVariable<CSSColorValue>("md-sys-color-surface-variant")
+    val mdSysColorOnSurfaceVariant = CSSStyleVariable<CSSColorValue>("md-sys-color-on-surface-variant")
+    val mdSysColorInverseSurface = CSSStyleVariable<CSSColorValue>("md-sys-color-inverse-surface")
+    val mdSysColorInverseOnSurface = CSSStyleVariable<CSSColorValue>("md-sys-color-inverse-on-surface")
+    val mdSysColorInversePrimary = CSSStyleVariable<CSSColorValue>("md-sys-color-inverse-primary")
+    val mdSysColorShadow = CSSStyleVariable<CSSColorValue>("md-sys-color-shadow")
+    val mdSysColorSurfaceTint = CSSStyleVariable<CSSColorValue>("md-sys-color-surface-tint")
+    val mdSysColorOutlineVariant = CSSStyleVariable<CSSColorValue>("md-sys-color-outline-variant")
+    val mdSysColorScrim = CSSStyleVariable<CSSColorValue>("md-sys-color-scrim")
 }
