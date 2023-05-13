0b97a4c5913390a575f2acf314adcaac670482d8
mminke <m.minke@kataru.nl>
2023-05-13 11:17:07 +0200

    fix(navigationbar): Add BottomNavigationBar widget

--- example/src/jsMain/kotlin/web/compose/material3/example/ExampleNavigationBar.kt
+++ example/src/jsMain/kotlin/web/compose/material3/example/ExampleNavigationBar.kt
@@ -4,21 +4,17 @@ import androidx.compose.runtime.Composable
 import org.jetbrains.compose.web.css.AlignItems
 import org.jetbrains.compose.web.css.DisplayStyle
 import org.jetbrains.compose.web.css.JustifyContent
-import org.jetbrains.compose.web.css.Position
 import org.jetbrains.compose.web.css.alignItems
-import org.jetbrains.compose.web.css.bottom
 import org.jetbrains.compose.web.css.display
 import org.jetbrains.compose.web.css.gap
 import org.jetbrains.compose.web.css.justifyContent
-import org.jetbrains.compose.web.css.left
 import org.jetbrains.compose.web.css.padding
-import org.jetbrains.compose.web.css.position
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Text
+import web.compose.extras.BottomNavigationBar
 import web.compose.material3.ColorScheme
 import web.compose.material3.icon.Icon
-import web.compose.material3.navigationbar.NavigationBar
 import web.compose.material3.navigationtab.NavigationTab
 import web.compose.material3.navigationtab.label
 import web.compose.material3.slot
@@ -32,23 +28,17 @@ fun ExampleNavigationBar(
     darkColorScheme: ColorScheme,
     setCurrentColorScheme: (ColorScheme) -> Unit,
 ) {
-    NavigationBar({
-        style {
-            position(Position.Fixed)
-            left(0.px)
-            bottom(0.px)
-            property("z-index", 99999)
-        }
-    }) {
-        NavigationTab({ label = "Tab 1" }) {
+
+    BottomNavigationBar {
+        NavigationTab({ label = "Home" }) {
             Icon({ slot = "activeIcon" }) { Text("home") }
             Icon({ slot = "inactiveIcon" }) { Text("home") }
         }
-        NavigationTab({ label = "Tab 2" }) {
+        NavigationTab({ label = "Explore" }) {
             Icon({ slot = "activeIcon" }) { Text("public") }
             Icon({ slot = "inactiveIcon" }) { Text("public") }
         }
-        NavigationTab({ label = "Tab 3" }) {
+        NavigationTab({ label = "Edit" }) {
             Icon({ slot = "activeIcon" }) { Text("edit_note") }
             Icon({ slot = "inactiveIcon" }) { Text("edit_note") }
         }
---
+++ library/src/jsMain/kotlin/web/compose/extras/BottomNavigationBar.kt
@@ -0,0 +1,33 @@
+package web.compose.extras
+
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.web.css.Color
+import org.jetbrains.compose.web.css.Position
+import org.jetbrains.compose.web.css.backgroundColor
+import org.jetbrains.compose.web.css.bottom
+import org.jetbrains.compose.web.css.left
+import org.jetbrains.compose.web.css.position
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.dom.AttrBuilderContext
+import org.jetbrains.compose.web.dom.ContentBuilder
+import web.compose.material3.navigationbar.NavigationBar
+import web.compose.material3.navigationbar.NavigationBarElement
+
+@Composable
+fun BottomNavigationBar(
+    attrs: AttrBuilderContext<NavigationBarElement>? = null,
+    content: ContentBuilder<NavigationBarElement>? = null
+) {
+    NavigationBar({
+        style {
+            position(Position.Fixed)
+            left(0.px)
+            bottom(0.px)
+            property("z-index", 99999)
+            backgroundColor(Color.red)
+        }
+        if (attrs != null) {
+            this.attrs()
+        }
+    }, content)
+}
