d35a72717d0f9134aeee74bd1d576b9b9e0041a7
mminke <m.minke@kataru.nl>
2023-05-27 20:29:30 +0200

    feat(elevation): Properly use the correct css variable for setting the elevation level

--- library/src/jsMain/kotlin/web/compose/material3/elevation/Elevation.kt
+++ library/src/jsMain/kotlin/web/compose/material3/elevation/Elevation.kt
@@ -1,8 +1,9 @@
 package web.compose.material3.elevation
 
 import androidx.compose.runtime.Composable
+import org.jetbrains.compose.web.css.CSSStyleVariable
+import org.jetbrains.compose.web.css.StylePropertyNumber
 import org.jetbrains.compose.web.dom.AttrBuilderContext
-import org.jetbrains.compose.web.dom.ContentBuilder
 import org.jetbrains.compose.web.dom.ElementBuilder
 import org.jetbrains.compose.web.dom.TagElement
 import web.compose.material3.ElementBuilderImplementation
@@ -12,16 +13,20 @@ import web.compose.material3.jsRequire
 abstract class ElevationElement : MdElement()
 
 @Composable
+fun Elevation(level: Int) = Elevation { style { mdElevationLevel(level) } }
+
+@Composable
 fun Elevation(
     attrs: AttrBuilderContext<ElevationElement>? = null,
-    content: ContentBuilder<ElevationElement>? = null
 ) = TagElement(
     elementBuilder = ElevationElementBuilder,
     applyAttrs = attrs,
-    content = content
+    content = null
 )
 
 private val ElevationElementBuilder: ElementBuilder<ElevationElement> =
     ElementBuilderImplementation<ElevationElement>("md-elevation").also {
         jsRequire("@material/web/elevation/elevation.js")
     }
+
+val mdElevationLevel = CSSStyleVariable<StylePropertyNumber>("--md-elevation-level")
