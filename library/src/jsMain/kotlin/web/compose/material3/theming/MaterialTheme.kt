f0ec9512471893fa0ce47237a6607cf4b428b96c
mminke <m.minke@kataru.nl>
2023-05-27 20:35:30 +0200

    chore(theming): Use more idiomatic stylesheet configuration for default background style

--- library/src/jsMain/kotlin/web/compose/material3/theming/MaterialTheme.kt
+++ library/src/jsMain/kotlin/web/compose/material3/theming/MaterialTheme.kt
@@ -1,7 +1,11 @@
 package web.compose.material3.theming
 
 import androidx.compose.runtime.Composable
-import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.css.Color
+import org.jetbrains.compose.web.css.Style
+import org.jetbrains.compose.web.css.StyleSheet
+import org.jetbrains.compose.web.css.backgroundColor
+import org.jetbrains.compose.web.css.value
 
 @Composable
 fun MaterialTheme(
@@ -11,15 +15,19 @@ fun MaterialTheme(
     if (colorScheme != null) {
         Style(colorScheme.asStylesheet())
     } else {
-        Style(StyleSheet().apply {
-            root style {
-                backgroundColor(MdSysColorVariables.mdSysColorSurfaceContainerLowest.value(Color("#FFFFFF")))
-            }
-        })
+        Style(defaultBackground)
     }
     if (fontScheme != null) {
         Style(fontScheme.asStyleSheet())
     }
 
     content()
+}
+
+private object defaultBackground: StyleSheet() {
+    init {
+        root style {
+            backgroundColor(MdSysColorVariables.mdSysColorSurfaceContainerLowest.value(Color("#FFFFFF")))
+        }
+    }
 }
