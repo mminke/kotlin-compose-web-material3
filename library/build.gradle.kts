b14ee4c46ee07af30f71a715928d7023137ca2f8
mminke <m.minke@kataru.nl>
2023-10-26 21:19:07 +0200

    build: try to publish artifacts

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -52,14 +52,10 @@ publishing {
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/mminke/kotlin-compose-web-material3")
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
-            }
         }
     }
     publications {
-        register<MavenPublication>("kotlin-compose-web-material3") {
+        create<MavenPublication>("kotlin-compose-web-material3") {
             from(components["java"])
         }
     }
